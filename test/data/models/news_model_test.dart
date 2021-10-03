9bb36ca9b1596279d200d343ea09d93758966b38
Joao Armando <joaoaarmando@hotmail.com>
2021-10-03 00:14:23 -0300

    Should set a place holder in case urlToImage is null

--- test/data/models/news_model_test.dart
+++ test/data/models/news_model_test.dart
@@ -35,4 +35,31 @@ void main() {
     expect(json["url"], newsModel.url);
     expect(json["publishedAt"], newsModel.publishedAt.toIso8601String());
   });
+
+  test('Should convert a newsModel into a NewsEntity', () {
+    final newsModel = NewsModel.fromJson(newsJson);
+
+    final newsEntity = newsModel.toEntity();
+
+    expect(newsEntity.source, newsModel.source);
+    expect(newsEntity.author, newsModel.author);
+    expect(newsEntity.title, newsModel.title);
+    expect(newsEntity.description, newsModel.description);
+    expect(newsEntity.url, newsModel.url);
+    expect(newsEntity.publishedAt, newsModel.publishedAt);
+  });
+
+  test('Should set a place holder in case urlToImage is null', () {
+    final newsModel = NewsModel.fromJson(newsJson..["urlToImage"] = null);
+
+    final newsEntity = newsModel.toEntity();
+
+    expect(newsEntity.source, newsModel.source);
+    expect(newsEntity.author, newsModel.author);
+    expect(newsEntity.title, newsModel.title);
+    expect(newsEntity.description, newsModel.description);
+    expect(newsEntity.url, newsModel.url);
+    expect(newsEntity.urlToImage, "https://awlights.com/wp-content/uploads/sites/31/2017/05/placeholder-news.jpg");
+    expect(newsEntity.publishedAt, newsModel.publishedAt);
+  });
 }
